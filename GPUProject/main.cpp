19c13c3722bc4b48b2225565ca7c4463266183de
Unknown <giuseppe.ellena@studenti.unimi.it>
2018-01-03 19:39:42 +0100

    update configurations file

--- GPUProject/main.cpp
+++ GPUProject/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+	clock_t total_start;
 	string line;
 	clock_t start;
 	double duration;
@@ -17,46 +18,46 @@ int main(int argc, char** argv) {
 	ofstream writeInFileB;
 	ofstream writeInFileT;
 	writeInFileB.open("benchmarkerCpu.txt");
-	writeInFileT.open("benchmarkerTimeCpu.txt");
+	//writeInFileT.open("benchmarkerTimeCpu.txt");
 	int solution = 0;
 	Solver solver=Solver();
 
 	if (testFile.is_open()) {
 		int i = 0;
+		total_start = clock();
 		while (getline(testFile, line)) {
 			/*if (i != 53) {
 				i++;
 				continue;
-			}*/
-			start = clock();
+			}*/		
 			Configuration c = Configuration(line);
 			writeInFileB << c;
-			solution = solver.FirstSevenMove(c);
-		
+			start = clock();
+			solution = solver.FirstSevenMove(c);		
 			if (solution == 0)
 			{		
-			solution = solver.Pvs(c, 6, numeric_limits<int>::min(), numeric_limits<int>::max());
+			solution = solver.Pvs(c, 10, numeric_limits<int>::min(), numeric_limits<int>::max());
 			if (!(solution % 2 == 0))
 				solution = -solution;
 			}
 			duration = (clock() - start) / (double)CLOCKS_PER_SEC;
-			writeInFileT <<i<<" "<<duration << endl;
+			//writeInFileT <<i<<" "<<duration << endl;
 			writeInFileB << "Configuration Number: " << i << endl;
 			writeInFileB << "Duration: " << duration << endl;
 			writeInFileB << "Number Of Turn Until Some Win: " << solution << endl;
-			writeInFileB << "Number Of Nodes Calculated: " << solver.getNodeCount() << endl;
+			//writeInFileB << "Number Of Nodes Calculated: " << solver.getNodeCount() << endl;
 			writeInFileB << "________________________________" << endl;
 			solver.ResetNodeCount();
 			i++;
-			if (i >500)
+			if (i >250)
 				break;
 			c.deleteBoard();
 		}
 		testFile.close();
 		writeInFileB.close();
-		writeInFileT.close();
+		//writeInFileT.close();
 	}
-
+	cout << ((clock() - total_start) / (double)CLOCKS_PER_SEC) << endl;
 	system("pause");
 	return 0;
 }
